0d5c3912a00efd1897598f933fb1c6901cad528f
ByBretema <bybretema@gmail.com>
2026-05-20 12:06:51 +0200

    updates

--- linux/install.sh
+++ linux/install.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash
 
 script_path=$(cd -- "$(dirname -- "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)
 source "${script_path}/scripts/bash/.bash_common"
-set -e
-
 
 ###############################################################################
 # Consts
@@ -111,7 +109,9 @@ link_config_files_and_themes() {
 
     # Yazi : https://github.com/yazi-rs/flavors/blob/main/themes.md
     dst_dir=$(mkdir_ret "${config_path}/yazi")
-    ln -srf "${my_configs}/yazi/themes/gruvbox_dark.toml" "${dst_dir}/theme.toml"
+    mkdir -p "${config_path}/yazi/flavors"
+    ya pkg add yazi-rs/flavors:catppuccin-mocha > /dev/null 2&>1 && ya pkg install || true
+    ln -srf "${my_configs}/yazi/themes/theme.toml" "${dst_dir}/theme.toml"
 
     #! Wallpapers
     log_header "Linking Wallpapers"
